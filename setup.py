6d714a2939184502e435604de28348986f29eef5
Oscar V <oscar.valenzuela.b@gmail.com>
2024-09-06 12:45:42 -0700

    cleaning

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-<<<<<<< HEAD
     name="xmonkey-lidy",
     version="1.0.0",
     description="A XMonkey tool for identifying SPDX licenses.",
@@ -25,20 +24,4 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
-=======
-    name='xmonkey_lidy',
-    version='0.1.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'click',
-        'nltk',
-        'scikit-learn',
-    ],
-    entry_points={
-        'console_scripts': [
-            'xmonkey-lidy = xmonkey_lidy.cli:main',
-        ],
-    },
->>>>>>> main
 )
--- xmonkey_lidy/model.py
+++
@@ -1,9 +0,0 @@
-def tag_paragraph(paragraph, config):
-    # Simple tagging based on keyword presence
-    for obligation in config['obligations']:
-        if obligation in paragraph:
-            return {'text': paragraph, 'tag': 'obligation'}
-    for grant in config['grants']:
-        if grant in paragraph:
-            return {'text': paragraph, 'tag': 'grant'}
-    return None
