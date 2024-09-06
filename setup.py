77606b03b8d378b07c5605e05e2031ca7c784af2
Oscar V <oscar.valenzuela.b@gmail.com>
2024-09-06 12:59:02 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xmonkey-lidy",
-    version="1.0.0",
+    version="1.0.1",
     description="A XMonkey tool for identifying SPDX licenses.",
     author="Oscar Valenzuela B.",
     packages=find_packages(),
