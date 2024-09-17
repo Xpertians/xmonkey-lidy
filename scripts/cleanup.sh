f3e9391ce90d79e8e584ef22171d65a2fcef6232
Oscar V <oscar.valenzuela.b@gmail.com>
2024-09-17 15:50:18 -0700

    cleanup licenses

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define the list of strings to be removed (without trailing commas in the array)
 strings_to_remove=(
   "use"
   "ipa"
@@ -14,8 +13,6 @@ strings_to_remove=(
   "permission"
 )
 
-# Loop through each string and apply the sed command
 for str in "${strings_to_remove[@]}"; do
-  # Escape the string properly for sed (double quotes and comma after each string)
   sed -i '' "/\"${str}\",/d" ./xmonkey_lidy/data/spdx_exclusions.json
 done
