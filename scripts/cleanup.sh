75d9e95c63e3d30e0fd9efa48ea2c036c43fdc62
Oscar V <oscar.valenzuela.b@gmail.com>
2024-09-17 15:30:18 -0700

    cleanup improvements

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Define the list of strings to be removed
+# Define the list of strings to be removed (without trailing commas in the array)
 strings_to_remove=(
-  "icu",
-  "use",
-  "ipa",
-  "cube",
-  "copyright",
-  "modification",
-  "derivative",
-  "software",
-  "license",
+  "use"
+  "ipa"
+  "cube"
+  "copyright"
+  "modification"
+  "derivative"
+  "software"
+  "license"
+  "warranty"
+  "permission"
 )
 
 # Loop through each string and apply the sed command
 for str in "${strings_to_remove[@]}"; do
-  sed -i '' "/$str/d" ./xmonkey_lidy/data/spdx_exclusions.json
+  # Escape the string properly for sed (double quotes and comma after each string)
+  sed -i '' "/\"${str}\",/d" ./xmonkey_lidy/data/spdx_exclusions.json
 done
