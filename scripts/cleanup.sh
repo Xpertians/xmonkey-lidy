f5483cf3dfe846fa2ca1280653a87a474910b5f8
Oscar V <oscar.valenzuela.b@gmail.com>
2024-09-17 15:17:56 -0700

    cleanup info

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,3 +2,5 @@
 
 sed -i '' '/"icu",/d' ./xmonkey_lidy/data/spdx_exclusions.json
 sed -i '' '/"use",/d' ./xmonkey_lidy/data/spdx_exclusions.json
+sed -i '' '/"ipa",/d' ./xmonkey_lidy/data/spdx_exclusions.json
+sed -i '' '/"cube",/d' ./xmonkey_lidy/data/spdx_exclusions.json
